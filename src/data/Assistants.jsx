91fcc0bea9b625cf0d054ddb670ff0ae86a937dc
Gonza <169013687+gonzaloavilacartes@users.noreply.github.com>
2025-06-23 03:34:28 -0400

    data y assets (avatares)

---
+++ src/data/Assistants.jsx
@@ -0,0 +1,18 @@
+export const assistants = [
+    {
+        id: 1,
+        name: "Valentina",
+        image: "/avatares/valentina.png",
+        category: "Experta Pokemon",
+        prompt: "Desde ahora eres un asistente virtual especializado en Pokemones.",
+        description: "Converso contigo de forma empática y cercana para ayudarte a reflexionar sobre los pensamientos que tiene los pokemones hacia a ti. 💬"
+    },
+    {
+        id: 2,
+        name: "Sebastian",
+        image: "/avatares/sebastian.png",
+        category: "Maestro Pokemon",
+        prompt: "Desde ahora eres un asistente virtual especializado en Pokemones.",
+        description: "Soy tu compañero pokemon: te hablo de pokemones y te acompañare en tu aventura pokemon"
+    }
+];
